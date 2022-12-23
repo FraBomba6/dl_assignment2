d5082d402a806181193c006fa1a9298b9ac4a1c7
canta2899 <andreacantarutti@icloud.com>
2022-12-23 22:29:28 +0100

    feat: validation split

--- src/main.py
+++ src/main.py
@@ -7,4 +7,6 @@ train_file = os.path.join(utils.PROJECT_ROOT, "data", "test.jsonl")
 
 train_data = utils.load_train(train_file)
 
+train, validation = utils.apply_random_validation_split(train_data)
+
 # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import torch.utils.data
+import torch.backends.mps
 import torch
 import json
 import os
 
 # Defining project root in order to avoid relative paths
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Initializing torch device according to hardware available
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
@@ -15,3 +17,12 @@ def load_train(path):
     with open(path, "r") as f:
         output = [json.loads(line) for line in f.readlines()]
         return pd.DataFrame.from_records(output)
+
+def apply_random_validation_split(train_data, split=20):
+    units = train_data.shape[0] 
+    validation_size = split * units / 100
+    train_size = units - validation_size
+    assert(validation_size + train_size == units)
+
+    return torch.utils.data.random_split(train_data, [train_size, validation_size])
+
